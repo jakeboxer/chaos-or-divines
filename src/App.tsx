f967468d76fc66462d438f3f16468343cc0f5007
Jake Card <jacob.boxer@gmail.com>
2025-11-13 19:02:40 -0500

    Add images

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <img src="/images/chaos.webp" alt="Chaos Orb" />
+      <img src="/images/divine.webp" alt="Divine Orb" />
     </div>
   );
 }
